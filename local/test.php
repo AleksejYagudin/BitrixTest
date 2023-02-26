3bee57d5601511206b83e785f63a093aa21fe84c
AleksejYagudin <49141634+AleksejYagudin@users.noreply.github.com>
2023-02-26 20:17:36 +0400

    select

--- local/test.php
+++ local/test.php
@@ -1,43 +1,25 @@
-<?php
-require_once($_SERVER['DOCUMENT_ROOT'] . "/bitrix/modules/main/include/prolog_before.php");
-//
-//
-//use Bitrix\Main\DI\ServiceLocator;
-//use Aclips\BookRental\Entity\Edition;
-//
-//$serviceLocator = ServiceLocator::getInstance();
-//
-//// Получение сервиса
-//$entityManager = $serviceLocator->get('aclips.bookrental.entityManager');
-//
-//// Создание объекта "издание"
-//$edition = new Edition();
-//
-//// Заполняем свойства объекта
-//$edition->name = 'Котенок Шмяк и большая 123';
-//$edition->publishingYear = 2020;
-//$edition->publishingHouseId = 1;
-//$edition->genreId = 3;
-//$edition->description = 'Приключения котёнка Шмяка...';
-//$edition->description123 = '2222';
-//
-//// Сохранение в базу
-//if($entityManager->save($edition)) {
-//    // Получаем объект из репозитория
-//    $processRepository = $entityManager->getRepository(Edition::class);
-//    $smyakEdition = $processRepository->getById($edition->id);
-//
-//    print "Название публикации: ".$smyakEdition->name;
-//    // Название публикации: Котенок Шмяк и большая тыква
-//
-//}
+<?php require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');?>
+<?php \Bitrix\Main\UI\Extension::load('aclips.ui-selector') ?>
 
-Bitrix\Main\Loader::includeModule('crm');
+<select id='my_select'>
+    <option data-tab='tab_1' value='1'>Option 1</option>
+    <option data-tab='tab_2' value='2'>Option 2</option>
+    <option data-tab='tab_3' value='3'>Option 3</option>
+    <option data-tab='tab_4' selected value='4'>Option 4</option>
+    <option data-tab='tab_4' value='5'>Option 5</option>
+</select>
 
-$row = \Bitrix\Crm\DealTable::getRow([
-    'filter' => [
-        'ID' => 4
-    ]
-]);
+<script type="text/javascript">
 
-print("\nid = " . $row['ID']);
+    BX.ready(function () {
+        BX.Plugin.UiSelector.createTagSelector('my_select', {
+            tabs: [
+                {id: 'tab_1', title: 'Tab 1', itemOrder: {title: 'asc'}},
+                {id: 'tab_2', title: 'Tab 2', itemOrder: {title: 'asc'}},
+                {id: 'tab_3', title: 'Tab 3', itemOrder: {title: 'asc'}},
+                {id: 'tab_4', title: 'Tab 4', itemOrder: {title: 'asc'}}
+            ]
+        })
+    })
+</script>
+<?php require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');?>
